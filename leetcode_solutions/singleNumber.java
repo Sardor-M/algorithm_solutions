a61266a641029badf78d392e79387e7f520733ea
Sardor-M <sardor0968@g.seoultech.ac.kr>
2024-11-03 16:22:24 +0900

    single number solution: added

---
+++ leetcode_solutions/singleNumber.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * Single Number.
+ * 
+ * Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+ * You must implement a solution with a linear runtime complexity and use only constant extra space.
+ * 
+ */
+
+public class singleNumber {
+    public int singleNumber(int [] nums) {
+        int singleOne = 0;
+
+        // this will give us the only number that appears once
+        for(int num: nums) {
+            singleOne ^= num;
+        }
+
+        return singleOne;
+    }
+}
