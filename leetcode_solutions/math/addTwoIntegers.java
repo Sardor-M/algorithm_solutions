82e95f01dc71bd5e5496e3c337a4d03751239d94
Sardor-M <sardor0968@g.seoultech.ac.kr>
2024-11-14 11:11:40 +0900

    add two integers: added

---
+++ leetcode_solutions/math/addTwoIntegers.java
@@ -0,0 +1,15 @@
+package leetcode_solutions.math;
+
+/**
+ * 
+ * Add Two Integers
+ * 
+ * Given two integers num1 and num2, return the sum of the two integers.
+ * 
+ **/
+
+public class addTwoIntegers {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
