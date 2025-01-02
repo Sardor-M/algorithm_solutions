2b5fbeddd90f8d6f41718371bdb3700b4ae050da
Sardor-M <sardor0968@g.seoultech.ac.kr>
2025-01-02 22:02:08 +0900

    minor typos fixed

--- leetcode_solutions/js/samTree.js
+++ leetcode_solutions/js/samTree.js
@@ -5,23 +5,20 @@
  *  
  */
 
-function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
-    // BFS uchun 2ta queue ishlatamiz 
-    const queue1: (TreeNode | null)[] = [q];
-    const queue2: (TreeNode | null)[] = [p];
+function isSameTree(p, q){
+    const queue1 = [p];
+    const queue2 = [q];
 
     while(queue1.length > 0 && queue2.length > 0){
         const node1 = queue1.shift();
         const node2 = queue2.shift();
 
         if(!node1 && !node2) continue;
-        // agar bittasi null yoki val lari mos kelmasa tree bir xil emas
         if(!node1 || !node2 || node1.val !== node2.val) return false;
 
-        // har ikkala tomoniga chap va ong tomondagi nodelarni queuega qoshamiz
-        queue1.push(node1.left, node1.right);
-        queue2.push(node2.left, node2.right);
-
+        queue1.push(node1.left, node1.rigt);
+        queue2.push(node2.left, node2.rigt);
     }
+
     return queue1.length === 0 && queue2.length === 0;
 }
