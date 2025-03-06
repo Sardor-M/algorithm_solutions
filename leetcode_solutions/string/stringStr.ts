31770a178e3662197935aef69b3af0364cd674fe
Sardor-M <sardor0968@g.seoultech.ac.kr>
2025-03-06 22:52:26 +0900

    index of first occurence in string solution added

---
+++ leetcode_solutions/string/stringStr.ts
@@ -0,0 +1,18 @@
+function strStr(haystack: string, needle: string): number{
+    if(needle.length === 0) return 0;
+
+    for(let i = 0; i <= haystack.length - needle.length; i++){
+        let found = true;
+
+        for(let j = 0; j < needle.length; j++){
+            if(haystack[i] !== needle[j]){
+                found = false;
+                break;
+            }
+        }
+        if(found){
+            return i;
+        }
+    }
+    return -1;
+}
