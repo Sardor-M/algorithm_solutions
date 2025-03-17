24209308fbeb7bf0e16aef766a321f98cac7f1ae
Sardor-M <sardor0968@g.seoultech.ac.kr>
2025-03-17 15:08:23 +0900

    employees avg experience solution added

---
+++ leetcode_solutions/sql_50/projectEmployees.sql
@@ -0,0 +1,7 @@
+SELECT 
+    p.project_id,
+    ROUND(AVG(e.experience_years), 2) as average_years
+FROM Employee e
+    JOIN Project p ON e.employee_id = p.employee_id
+GROUP BY 
+    p.project_id;
