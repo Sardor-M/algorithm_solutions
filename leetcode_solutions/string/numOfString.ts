ca589d6e374d999303076d8aec6ad1ec74fdd593
Sardor-M <sardor0968@g.seoultech.ac.kr>
2025-03-21 06:13:21 +0900

    num of strings solution added

---
+++ leetcode_solutions/string/numOfString.ts
@@ -0,0 +1,10 @@
+function numOfStrings(patterns: string[], word: string): number {
+    let count: number = 0;
+
+    for(const pattern of patterns){
+        if(word.includes(pattern)){
+            count++;
+        } 
+    }
+    return count;
+};
