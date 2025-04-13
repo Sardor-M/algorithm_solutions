5b7f4a95ba3da61492516640926cf65ef1703dbd
Sardor-M <sardor0968@g.seoultech.ac.kr>
2025-04-13 15:57:36 +0900

    unique length-3 palindrome subsequence solution added

---
+++ leetcode_solutions/string/uniquLength3Palindrome.ts
@@ -0,0 +1,29 @@
+function countPalindromicSubsequence(s: string): number {
+    const first = {};
+    const last = {};
+
+    // we first get the first and last occurence 
+    for(let i = 0; i < s.length; i++){
+        const char = s[i];
+        if(first[char] === undefined){
+            first[char] = i;
+        }
+        last[char] = i;
+    }
+
+    let count = 0;
+    // unique characterlarni iterate qilamiz (from first)
+    for(const char in first){
+        if(first[char] < last[char]){
+            const uniqMiddleChars = new Set();
+            // first and last character occurence orasida iterate qilamiz
+            for(let i = first[char] + 1; i < last[char]; i++){
+                uniqMiddleChars.add(s[i]);
+            }
+
+            // we add the size of set to the count
+            count += uniqMiddleChars.size;
+        }
+    }
+    return count;
+};
